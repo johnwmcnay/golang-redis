33bdabc48cec2b5e7c8f988a14755d7f8de83e19
johnwmcnay <john.w.mcnay@gmail.com>
2021-01-02 17:04:00 -0600

    global client variable set once in main

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ type Article struct {
 	Content string `json:"Content"`
 }
 
-var Articles []Article
+var client radix.Client
 
 func main() {
 
@@ -28,20 +28,15 @@ func main() {
 		)
 	}
 
-
+	var err error
 	// this pool will use our ConnFunc for all connections it creates
-	client, err := radix.NewPool("tcp", "127.0.0.1:6379", 10, radix.PoolConnFunc(customConnFunc))
+	client, err = radix.NewPool("tcp", "127.0.0.1:6379", 10, radix.PoolConnFunc(customConnFunc))
 
-	err = client.Do(radix.Cmd(nil, "SET", "foo", "someval"))
 	if err != nil {
 		// handle error
 	}
-
 	fmt.Println("Rest API v2.0 - Mux Routers")
-	Articles = []Article{
-		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-	}
+
 	handleRequests()
 }
 
@@ -52,7 +47,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllArticles")
-	json.NewEncoder(w).Encode(Articles)
+	//json.NewEncoder(w).Encode(Articles)
 }
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
@@ -70,36 +65,36 @@ func updateArticle(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Endpoint Hit: updateArticle")
 
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	var article Article
-	vars := mux.Vars(r)
-	key := vars["id"]
-
-	json.Unmarshal(reqBody, &article)
-	// update our global Articles array to include
-	// our new Article
-	for index, art := range Articles {
-		if art.Id == key {
-			Articles[index] = article
-			json.NewEncoder(w).Encode(article)
-			break
-		}
-	}
+	//reqBody, _ := ioutil.ReadAll(r.Body)
+	//var article Article
+	//vars := mux.Vars(r)
+	//key := vars["id"]
+	//
+	//json.Unmarshal(reqBody, &article)
+	//// update our global Articles array to include
+	//// our new Article
+	//for index, art := range Articles {
+	//	if art.Id == key {
+	//		Articles[index] = article
+	//		json.NewEncoder(w).Encode(article)
+	//		break
+	//	}
+	//}
 }
 
 func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["id"]
-
-	fmt.Println("Endpoint Hit: returnSingleArticle")
-	// Loop over all of our Articles
-	// if the article.Id equals the key we pass in
-	// return the article encoded as JSON
-	for _, article := range Articles {
-		if article.Id == key {
-			json.NewEncoder(w).Encode(article)
-		}
-	}
+	//vars := mux.Vars(r)
+	//key := vars["id"]
+	//
+	//fmt.Println("Endpoint Hit: returnSingleArticle")
+	//// Loop over all of our Articles
+	//// if the article.Id equals the key we pass in
+	//// return the article encoded as JSON
+	//for _, article := range Articles {
+	//	if article.Id == key {
+	//		json.NewEncoder(w).Encode(article)
+	//	}
+	//}
 }
 
 func createNewArticle(w http.ResponseWriter, r *http.Request) {
@@ -112,29 +107,31 @@ func createNewArticle(w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal(reqBody, &article)
 	// update our global Articles array to include
 	// our new Article
-	Articles = append(Articles, article)
+	err := client.Do(radix.Cmd(nil, "SET", "article" , "test"))
+	if err != nil {
 
+	}
 	json.NewEncoder(w).Encode(article)
 }
 
 func deleteArticle(w http.ResponseWriter, r *http.Request) {
 	// once again, we will need to parse the path parameters
-	vars := mux.Vars(r)
-	// we will need to extract the `id` of the article we
-	// wish to delete
-	id := vars["id"]
-
-	fmt.Println("Endpoint Hit: Delete Article")
-
-	// we then need to loop through all our articles
-	for index, article := range Articles {
-		// if our id path parameter matches one of our
-		// articles
-		if article.Id == id {
-			// updates our Articles array to remove the
-			// article
-			Articles = append(Articles[:index], Articles[index+1:]...)
-			break
-		}
-	}
+	//vars := mux.Vars(r)
+	//// we will need to extract the `id` of the article we
+	//// wish to delete
+	//id := vars["id"]
+	//
+	//fmt.Println("Endpoint Hit: Delete Article")
+	//
+	//// we then need to loop through all our articles
+	//for index, article := range Articles {
+	//	// if our id path parameter matches one of our
+	//	// articles
+	//	if article.Id == id {
+	//		// updates our Articles array to remove the
+	//		// article
+	//		Articles = append(Articles[:index], Articles[index+1:]...)
+	//		break
+	//	}
+	//}
 }
