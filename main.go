50a80b9541fc750a8da6c8078dbe0fe226470076
johnwmcnay <john.w.mcnay@gmail.com>
2021-01-02 15:27:01 -0600

    CRUD functionality in Go example

--- main.go
+++ main.go
@@ -4,20 +4,28 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 type Article struct {
-	Title string `json:"Title"`
-	Desc string `json:"desc"`
-	Content string `json:"content"`
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"Desc"`
+	Content string `json:"Content"`
 }
 
-// let's declare a global Articles array
-// that we can then populate in our main function
-// to simulate a database
 var Articles []Article
+
+func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	Articles = []Article{
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	handleRequests()
+}
 
 func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -30,23 +38,87 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(Articles)
 }
 func handleRequests() {
-	// creates a new instance of a mux router
 	myRouter := mux.NewRouter().StrictSlash(true)
-	// replace http.HandleFunc with myRouter.HandleFunc
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/all", returnAllArticles)
-	// finally, instead of passing in nil, we want
-	// to pass in our newly created router as the second
-	// argument
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/article/{id}", updateArticle).Methods("PUT")
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
-func main() {
-	fmt.Println("Rest API v2.0 - Mux Routers")
-	Articles = []Article{
-		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+func updateArticle(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Endpoint Hit: updateArticle")
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	json.Unmarshal(reqBody, &article)
+	// update our global Articles array to include
+	// our new Article
+	for index, art := range Articles {
+		if art.Id == key {
+			Articles[index] = article
+			json.NewEncoder(w).Encode(article)
+			break
+		}
 	}
-	handleRequests()
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	fmt.Println("Endpoint Hit: returnSingleArticle")
+	// Loop over all of our Articles
+	// if the article.Id equals the key we pass in
+	// return the article encoded as JSON
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: createNewArticle")
+	// get the body of our POST request
+	// unmarshal this into a new Article struct
+	// append this to our Articles array.
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+	// update our global Articles array to include
+	// our new Article
+	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	// once again, we will need to parse the path parameters
+	vars := mux.Vars(r)
+	// we will need to extract the `id` of the article we
+	// wish to delete
+	id := vars["id"]
+
+	fmt.Println("Endpoint Hit: Delete Article")
+
+	// we then need to loop through all our articles
+	for index, article := range Articles {
+		// if our id path parameter matches one of our
+		// articles
+		if article.Id == id {
+			// updates our Articles array to remove the
+			// article
+			Articles = append(Articles[:index], Articles[index+1:]...)
+			break
+		}
+	}
 }
 
